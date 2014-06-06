d382f7f559d4af86334bd9a6b29856c092a28a2e
wooorm <tituswormer@gmail.com>
2014-06-06 13:56:45 +0200

    Fixed JSCodeStyle in spec/abbreviation.decimal.tld

--- spec/parse-english.spec.parser.abbreviation.prefix.decimal.js
+++ spec/parse-english.spec.parser.abbreviation.prefix.decimal.js
@@ -2,25 +2,60 @@ var converter = require('..'),
     assert = require('assert');
 
 describe('Abbreviations: Decimals (prefixed by a full-stop)', function () {
-    it('should *not* treat the dot-character preceding decimals (e.g., `1`, `2`, &c.), as a terminal marker', function () {
-        var digits = '0123456789',
-            root;
-
-        digits.split('').forEach(function (digit) {
-            root = converter('See § .' + digit + ' ¶ 2.');
-            assert(root.toAST() === JSON.stringify({'type':'RootNode','children':[{'type':'ParagraphNode','children':[{'type':'SentenceNode','children':[
-                { 'type': 'WordNode', 'value': 'See' },
-                { 'type': 'WhiteSpaceNode', 'value': ' ' },
-                { 'type': 'PunctuationNode', 'value': '§' },
-                { 'type': 'WhiteSpaceNode', 'value': ' ' },
-                { 'type': 'PunctuationNode', 'value': '.' },
-                { 'type': 'WordNode', 'value': digit },
-                { 'type': 'WhiteSpaceNode', 'value': ' ' },
-                { 'type': 'PunctuationNode', 'value': '¶' },
-                { 'type': 'WhiteSpaceNode', 'value': ' ' },
-                { 'type': 'WordNode', 'value': '2' },
-                { 'type': 'PunctuationNode', 'value': '.' }
-            ]}]}]}));
-        });
-    });
+    it('should *not* treat the dot-character preceding decimals (e.g., ' +
+        '`1`, `2`, &c.), as a terminal marker', function () {
+            '0123456789'.split('').forEach(function (digit) {
+                var root = converter('See § .' + digit + ' ¶ 2.');
+                assert(root.head.head.toAST() === JSON.stringify({
+                    'type' : 'SentenceNode',
+                    'children' : [
+                        {
+                            'type' : 'WordNode',
+                            'value' : 'See'
+                        },
+                        {
+                            'type' : 'WhiteSpaceNode',
+                            'value' : ' '
+                        },
+                        {
+                            'type' : 'PunctuationNode',
+                            'value' : '§'
+                        },
+                        {
+                            'type' : 'WhiteSpaceNode',
+                            'value' : ' '
+                        },
+                        {
+                            'type' : 'PunctuationNode',
+                            'value' : '.'
+                        },
+                        {
+                            'type' : 'WordNode',
+                            'value' : digit
+                        },
+                        {
+                            'type' : 'WhiteSpaceNode',
+                            'value' : ' '
+                        },
+                        {
+                            'type' : 'PunctuationNode',
+                            'value' : '¶'
+                        },
+                        {
+                            'type' : 'WhiteSpaceNode',
+                            'value' : ' '
+                        },
+                        {
+                            'type' : 'WordNode',
+                            'value' : '2'
+                        },
+                        {
+                            'type' : 'PunctuationNode',
+                            'value' : '.'
+                        }
+                    ]
+                }));
+            });
+        }
+    );
 });
